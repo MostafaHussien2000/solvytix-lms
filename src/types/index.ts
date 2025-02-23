8d01c437b4ad32a9639e6ed00dbdc4ae932aeff4
MostafaHussien2000 <mabozeed70@gmail.com>
2025-02-23 23:57:50 +0200

    defining types

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface Course {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  numberOfHours: number;
+  banner: string;
+  trainerId?: string;
+}
+
+export interface Trainer {
+  id: string;
+  name: string;
+  bio: string;
+  image: string;
+  hourlyRate: number;
+  courses?: Course[];
+}
+
+export enum UserRole {
+  ADMIN,
+  TRAINER,
+}
+
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+  role: UserRole;
+}
