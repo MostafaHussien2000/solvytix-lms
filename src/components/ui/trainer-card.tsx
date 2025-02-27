4a412a3e1c63acc9d7a869904b36bef50442ca2f
MostafaHussien2000 <mabozeed70@gmail.com>
2025-02-27 20:51:25 +0200

    fixing trainer card

--- src/components/ui/trainer-card.tsx
+++ src/components/ui/trainer-card.tsx
@@ -4,10 +4,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 function TrainerCard({ trainer }: { trainer: Trainer }) {
   return (
-    <Card>
+    <Card className="overflow-hidden cursor-pointer transition-colors duration-300 ease-in-out border-2 hover:border-primary">
       <CardHeader>
         <div className="flex items-center gap-2">
-          <Avatar className="w-12 h-12 rounded-full flex justify-center items-center bg-muted-foreground">
+          <Avatar className="w-12 h-12 rounded-full flex justify-center items-center bg-muted-foreground overflow-hidden">
             <AvatarImage src={trainer.image} />
             <AvatarFallback>
               {trainer.name.charAt(0).toLocaleUpperCase()}
